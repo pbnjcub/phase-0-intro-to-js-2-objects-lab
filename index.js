d8342058b10f8ec8540e527b12326e40f3788b70
Paul Jung <pbnjcub@gmail.com>
2022-06-12 15:52:57 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Write your solution in this file!
+const employee = {
+    name: "Paul Jung",
+    streetAddress: {
+        street: "27 Main St.",
+        city: "Englewood",
+        state: "NJ",
+        zipCode: "07631",
+    }
+};
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+
+    newObj[key] = value;
+
+    return newObj;
+};
+
+const employeeTwo = updateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "Michelle Carstents"
+    );
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+};
+
+const employeeThree = destructivelyUpdateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "Mark Gagne"
+);
+
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj };
+    delete newObj[key];
+    return newObj;
+};
+
+const employeeFour = deleteFromEmployeeByKey (
+    employee,
+    "name"
+);
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+};
+
+const employeeFive = destructivelyDeleteFromEmployeeByKey(
+    employee,
+    "name"
+    );
